8923fc14dba3cb62873d06683343641bf0333a85
asibakatw <asibaka.9@gmail.com>
2023-06-23 22:20:39 +0700

    with openai service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import {
+  Configuration,
+  CreateCompletionResponseChoicesInner,
+  OpenAIApi,
+} from 'openai';
 
 @Injectable()
 export class AppService {
+  private openai: OpenAIApi;
+  constructor() {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_TOKEN,
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getResponse(): Promise<CreateCompletionResponseChoicesInner[]> {
+    const chat_completion = await this.openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [],
+      // prompt: generatePrompt('cats'),
+      temperature: 0.6,
+    });
+    return chat_completion.data.choices;
   }
 }
